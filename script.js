0980af5d4333da64464fd8bc57f123f1921c0fd8
tiburcioMartim <tiburciomartim@gmail.com>
2024-03-03 10:20:43 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var display = document.querySelector('#display');
+
+function showDisplay(value) {
+    if (value == '.') {
+        display.value = '0';
+    }
+    display.value += value;
+}
+
+function clearAll() {
+    display.value = '';
+}
+
+function result() {
+    let y = eval(display.value);
+    display.value = y;
+
+    if (display.value == 'undefined') {
+        display.value = '';
+    }
+}
